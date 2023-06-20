ce023049c721e9dc8cac1dbbbaa1ecf3385b80f4
SushamaDinde <62433968+SushamaDinde@users.noreply.github.com>
2023-06-20 16:22:12 +0200

    name change

--- terraform/storageaccount/main.tf
+++ terraform/storageaccount/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_resource_group" "rg-demo" {
 }
 
 resource "azurerm_storage_account" "sa-demo" {
-  name                     = "storageaccountname"
+  name                     = "sademo"
   resource_group_name      = azurerm_resource_group.rg-demo.name
   location                 = azurerm_resource_group.rg-demo.location
   account_tier             = "Standard"
