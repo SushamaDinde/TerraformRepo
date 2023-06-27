6fea2d5d1359831b4b9af3e263a1c473d38caf28
SushamaDinde <62433968+SushamaDinde@users.noreply.github.com>
2023-06-27 15:54:05 +0200

    storage account subnet added

--- terraform/storageaccount/main.tf
+++ terraform/storageaccount/main.tf
@@ -16,6 +16,12 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
+data "azurerm_subnet" "datasubnet" {
+  name                 = "storagesubnet"
+  virtual_network_name = "vnet-sushma-demo"
+  resource_group_name  = "rg-demo"
+}
+
 resource "azurerm_resource_group" "rg-demo" {
   name     = "rg-demo"
   location = "West Europe"
@@ -27,4 +33,7 @@ resource "azurerm_storage_account" "sa-demo" {
   location                 = azurerm_resource_group.rg-demo.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  network_rules {
+    virtual_network_subnet_ids = [data.azurerm_subnet.datasubnet.id]
+  }
 }
